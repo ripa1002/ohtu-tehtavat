1e7adf05f4a394150da101c1e0269c2d0e7274a7
Risto Sutinen <risto.sutinen@gmail.com>
2019-11-14 23:07:10 +0200

    Viikko 3 tehtävät 7-9

---
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -0,0 +1,70 @@
+package ohtu;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("http://localhost:4567");
+        
+        // tulostetaan sivu konsoliin
+        System.out.println(driver.getPageSource());
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("jouko");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("seppo");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("seppo");
+        
+        element = driver.findElement(By.name("signup"));
+        element.submit();
+        
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+ 
+        System.out.println(driver.getPageSource());
+        
+        /*
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+        
+        // tulostetaan sivu konsoliin
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        
+        // element.sendKeys("akk");
+        
+        System.out.println(driver.getPageSource());
+        
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println(driver.getPageSource());
+        */
+        
+        driver.quit();
+    }
+    
+}
